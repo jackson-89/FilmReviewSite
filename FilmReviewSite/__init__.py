a3f4f73d3d7cfb685c6525122eb88445cfd7d6f8
jackson-89 <ccc012105@coderacademy.edu.au>
2021-11-04 09:10:01 +1000

    made sqlalchemy routes

--- FilmReviewSite/__init__.py
+++ FilmReviewSite/__init__.py
@@ -1,18 +1,76 @@
-from flask import Flask
+import os
+from flask import Flask,request,jsonify
+from flask_sqlalchemy import SQLAlchemy
+import psycopg2
+
+(
+    db_user,
+    db_pass,
+    db_name,
+    db_domain
+) = (os.environ.get(item) for item in [
+    "DB_USER",
+    "DB_PASS",
+    "DB_NAME",
+    "DB_DOMAIN"
+    ]
+)
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "Hello, world! Check this out!"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql+psycopg2://{db_user}:{db_pass}@{db_domain}/{db_name}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-@app.route('/users/')
-def get_users():
-    return "This will be a list of all users that have made reviews"
+db = SQLAlchemy(app)
 
-@app.route('/users/<int:user_id>')
-def get_specific_user(user_id):
-    return f"This will be a page displaying info about a specific user about user {user_id}"
+class Review(db.Model):
+    __tablename__="reviews"
+    review_id = db.Column(db.Integer, primary_key=True)
+    review_name = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __init__(self, review_name):
+        self.review_name = review_name
+
+    @property
+    def serialize(self):
+        return {
+            "review_id": self.review_id,
+            "review_name":self.review_name
+        }    
+
+db.create_all()
+
+@app.route('/reviews/',methods=["GET"])
+def review_index():
+    reviews = Review.query.all()
+    return jsonify([review.serialize for review in reviews])
+
+@app.route('/reviews/',methods=["POST"])
+def create_review():
+    new_review=Review(request.json['review_name'])
+    db.session.add(new_review)
+    db.session.commit()
+    return jsonify(new_review.serialize)
+
+@app.route('/reviews/<int:id>/',methods=["GET"])
+def get_review(id):
+    review=Review.query.get_or_404(id)
+    return jsonify([review.serialize])
+
+@app.route("/reviews/<int:id>/", methods=["PUT","PATCH"])
+def update_review(id):
+    review=Review.query.filter_by(review_id=id)
+    review.update(dict(review_name=request.json["review_name"]))
+    db.session.commit()
+    return jsonify(review.first().serialize)
+
+@app.route("/reviews/<int:id>/",methods=["DELETE"])
+def delete_review(id):
+    review=Review.query.get_or_404(id)
+    db.session.delete(review)
+    db.session.commit()
+    return jsonify(review.serialize)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
